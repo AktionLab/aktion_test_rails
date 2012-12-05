eaf47a11dd7fe0cf69023cfcb951557677521b6b
Chris Boertien <chris@aktionlab.com>
2012-12-04 21:37:00 -0700

    drop uglifier and make database config jdbc friendly
    Signed-off-by: Chris Boertien <chris@aktionlab.com>

--- spec/spec_active_record.rb
+++ spec/spec_active_record.rb
@@ -2,7 +2,11 @@ require 'active_record'
 require 'rspec/rails/adapters'
 require 'rspec/rails/fixture_support'
 
-ActiveRecord::Base.establish_connection(adapter: 'sqlite3', database: 'tmp/test.sqlite3')
+if RUBY_PLATFORM =~ /java/
+  ActiveRecord::Base.establish_connection(adapter: 'jdbcsqlite3', database: 'tmp/test.sqlite3')
+else
+  ActiveRecord::Base.establish_connection(adapter: 'sqlite3', database: 'tmp/test.sqlite3')
+end
 
 RSpec.configure do |config|
   config.use_transactional_fixtures = true
