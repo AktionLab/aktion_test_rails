3cffdf172f839a0d0d4156c1e25836c8501794a5
Chris Boertien <chris@aktionlab.com>
2013-02-03 12:43:45 -0700

    manually setup simplecov as we're not actually profiling a rails app
    Signed-off-by: Chris Boertien <chris@aktionlab.com>

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -9,12 +9,20 @@ I18n.load_path << File.join(TESTAPP_ROOT, 'config/locales/devise.en.yml')
 
 require 'activeadmin'
 
+require 'simplecov'
+SimpleCov.start do
+  add_filter '/spec/'
+  add_group 'Modules', 'lib/aktion_test/module'
+  add_group 'Matchers', 'lib/aktion_test_rails/matchers'
+  add_group 'Support', 'lib/aktion_test_rails/support'
+end
+
 AktionTest::SpecHelper.build do
   use :Timecop, :Faker
   use :Rails, :path => File.expand_path('../rails_app/config/environment', __FILE__)
 
   scope 'Rails' do
-    use :Simplecov, :RSpec, :AktionTest, :ShouldaMatchers, :FactoryGirl, :Capybara
+    use :RSpec, :AktionTest, :ShouldaMatchers, :FactoryGirl, :Capybara
   end
 end
 
